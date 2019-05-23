3c2d6aa505d82acbf104b5a5c38b5a09e4fe97e9
David Smale <davidlsmale@gmail.com>
2019-05-23 23:06:06 +0100

    Additional comments added.

--- app.R
+++ app.R
@@ -3,28 +3,38 @@ library(tidyverse)
 library(DT)
 library(extrafont)
 
+# Pre-processing ----------------------------
+
+# Read in csv file of resources compiled by me
 reading_list <- read_csv("r_reading_list.csv")
 
+# Create string for hyperlinks
 reading_table <- reading_list %>%
   mutate(title_link =  paste0("<a href='",link,"' target='_blank'>",title,"</a>")) %>%
   select(title_link, everything(), -title, -link)
 
+# UI -------------------------------
 
 ui <- fluidPage(
+  # formatting with css
   includeCSS("styles.css"),
+  
   # Application title
   
   h2(toupper("Free R Reading Material")),
   
+  # Intro text
   p("A collection of books about the R programming language and Data Science, that you can read for free!"),
   
   p("If there is a book that you think I should add to the list, then please let me know",
     a("@committedtotape", href = "https://twitter.com/committedtotape")),
   
+  # DT table
   DTOutput("tbl"),
   
   br(),
   
+  # credits
   div(p("Compiled by", a("@committedtotape", href = "https://twitter.com/committedtotape"), "using the shiny and DT packages"), 
       p("Blog:", a("davidsmale.netlify.com", href = "https://davidsmale.netlify.com/portfolio/")),
       p("GitHub:", a("freeR", href = "https://github.com/committedtotape/freeR")),
@@ -36,10 +46,11 @@ ui <- fluidPage(
 
 
 
-# Define server logic required to draw a histogram
+# Server -------------------------------
 
 server <- function(input, output) {
   
+  # render DT table
   output$tbl <- renderDT({
     
     datatable(reading_table,
